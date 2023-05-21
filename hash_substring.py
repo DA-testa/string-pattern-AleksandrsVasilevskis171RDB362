15f2074be14a9bd804f47744654f39bbbeebdbc7
AleksandrsVasilevskis171RDB362 <125203034+AleksandrsVasilevskis171RDB362@users.noreply.github.com>
2023-05-21 15:14:18 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -30,7 +30,7 @@ def read_input():
         first_line= input()
         second_line = input()  
     elif check == True:
-        f = open("C:/do/a.txt",mode = "r")
+        f = open("tests/06",mode = "r")
         first_line=f.readline()
         second_line = f.readline()
 
